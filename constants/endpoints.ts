d3273a971183cc5c1a19403788acefc24dc991e6
Pradip Chapagain <pradipcpgn@gmail.com>
2025-03-20 03:20:19 +0545

    Fix:api url

--- constants/endpoints.ts
+++ constants/endpoints.ts
@@ -1,3 +1,4 @@
-export const BASE_URL="http://localgost:3001"
+//use ip address if api not hitting
+export const BASE_URL="http://localhost:3001"
 export const SCHEMES=BASE_URL+"/schemes"
 export const INVESTMENTS=BASE_URL+"/investments"
